c64446777dc47a495da7cc559ea004879d99bd0d
Mathias Rendon <mathiassaid7@gmail.com>
2025-01-08 18:59:37 -0500

    Add dark mode and list buttons to show some text

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+const washLi = document.getElementById('washLi')
+const washP = document.getElementById('washP')
+const walkLi = document.getElementById('walkLi')
+const walkP = document.getElementById('walkP')
+const manicureLi = document.getElementById('manicureLi')
+const manicureP = document.getElementById('manicureP')
+const daycareLi = document.getElementById('daycareLi')
+const daycareP = document.getElementById('daycareP')
+const moon = document.getElementById('moon')
+const sun = document.getElementById('sun')
+const body = document.body
+
+const showP = (li, p) => {
+    li.addEventListener('click', () => {
+        p.classList.toggle('active')
+    })
+}
+
+showP(washLi, washP)
+showP(walkLi, walkP)
+showP(manicureLi, manicureP)
+showP(daycareLi, daycareP)
+
+moon.addEventListener('click', () => {
+    body.classList.add('darkMode')
+})
+
+sun.addEventListener('click', () => {
+    body.classList.remove('darkMode')
+})
